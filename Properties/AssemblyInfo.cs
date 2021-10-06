366d354f0d944b664a10b37d263f23efd37b75dc
Nidhi <nidhi.g.solanki@gmail.com>
2021-10-06 15:22:24 +1100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeMonthlyPaySlip.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Computershare")]
+[assembly: AssemblyProduct("EmployeeMonthlyPaySlip.Tests")]
+[assembly: AssemblyCopyright("Copyright © Computershare 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f4caf49-211d-4dcc-a4a9-9026a9dccfca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
